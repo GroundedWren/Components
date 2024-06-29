5ccaf1c346ad0ce1d62400baafcc1c27a37fb7c5
Kathryn Aulabaugh <kathryn.aulabaugh@gmail.com>
2024-06-29 16:51:40 -0500

    Creates the shortcuts component

---
+++ Scripts/ShortcutsControl.js
@@ -0,0 +1,158 @@
+/**
+ * Author: Vera Konigin
+ * Site: https://groundedwren.neocities.org
+ * Contact: vera@groundedwren.com
+ * 
+ * File description: A component to register and manage shortcuts
+ */
+
+window.GW = window.GW || {};
+window.GW.Components = window.GW.Components || {};
+(function Shortcuts(ns) {
+
+    /** This creates the popover region for reporting shortcuts */
+    window.addEventListener("load", function shortcutsControlOnLoad() {
+        document.body.insertAdjacentHTML(
+            "beforeend",
+            `<aside id="gw-shortcuts-live" aria-live="polite" style="display: contents;"></aside>`
+        );
+        ns.AsiShortcutsLive = document.getElementById("gw-shortcuts-live");
+    });
+
+    /** Called as an event handler, this reports the shortcuts available to the current target element as a popover. */
+    ns.reportShortcuts = (event) => {
+        let curEl = event.target;
+        let shortsIndex = {};
+        while(curEl) {
+            if(curEl.tagName === "GW-SHORTCUTS") {
+                for(attrObj of Object.values(curEl.attrMap)) {
+                    if(!shortsIndex[attrObj.CODE]) {
+                        shortsIndex[attrObj.CODE] = attrObj.INFO;
+                    }
+                }
+            }
+            curEl = curEl.parentElement;
+        }
+        const popover = document.createElement("popover");
+        popover.setAttribute("popover", "auto");
+        popover.innerHTML = `<table><caption>Shortcuts</caption><tbody>${Object.keys(shortsIndex).sort().map(
+            CODE => `<tr><th scope="row">${CODE}</th><td>${shortsIndex[CODE]}</td></tr>`
+        ).join("")}</tbody></table>`;
+
+        ns.AsiShortcutsLive.innerHTML = "";
+        ns.AsiShortcutsLive.appendChild(popover);
+        popover.showPopover();
+    };
+
+    /**
+     * A custom element which enables shortcuts on all of its children
+     * There are three types of attributes which can be defined:
+     *   code - These are key sequences to activate a shortcut. E.g. "Alt+A"
+     *   handler - This is code to invoke for a shortcut.
+     *   info - This is a description of the shortcut to display to users.
+     * Each should be suffixed by an underscore and a number which associates the attributes together.
+     * E.g. <gw-shortcutscode_1="Alt+A" handler_1="alert('hi')" info_1="Says hello"></gw-shortcuts>
+     */
+    ns.ShortcutsEl = class ShortcutsEl extends HTMLElement {
+        //#region staticProperties
+        static instanceCount = 0;
+        static instanceMap = {};
+        //#endregion
+
+        //#region instance properties
+        instanceId;
+        initialized = false;
+        shortcutMap = {};
+        currentLevel = null;
+        //#endregion
+
+        constructor() {
+            super();
+            this.instanceId = ShortcutsEl.instanceCount++;
+            ShortcutsEl.instanceMap[this.instanceId] = this;
+
+            if(this.instanceId === 1) {
+                document.head.insertAdjacentHTML("beforeend",`
+                    <style>
+                        gw-shortcuts {
+                            display: contents;
+                        }
+                    </style>
+                `);
+            }
+        }
+        
+        get idKey() {
+            return `gw-shortcuts-${this.instanceId}`;
+        }
+
+        connectedCallback() {
+            if(!this.initialized) {
+                this.registerShorts();
+
+                const observer = new MutationObserver(() => this.registerShorts());
+                observer.observe(this, {attributes: true, childList: false, subtree: false});
+
+                this.addEventListener("keydown", this.onKeyDown);
+                this.addEventListener("keyup", this.onKeyUp);
+
+                this.initialized = true;
+            }
+        }
+
+        registerShorts() {
+            this.attrMap = {};
+            this.getAttributeNames().reduce((map, attr) => {
+                const attrPieces = attr.split("_");
+                if(attrPieces.length === 2
+                    && (
+                        attrPieces[0].toUpperCase() === "CODE"
+                        || attrPieces[0].toUpperCase() === "HANDLER"
+                        || attrPieces[0].toUpperCase() === "INFO"
+                    )
+                ) {
+                    map[attrPieces[1]] = map[attrPieces[1]] || {};
+                    map[attrPieces[1]][attrPieces[0].toUpperCase()] = this.getAttribute(attr);
+                }
+                return map;
+            }, this.attrMap);
+
+            this.shortcutMap = {};
+            for (let shortObj of Object.values(this.attrMap)) {
+                if(!shortObj.CODE || !shortObj.HANDLER) {
+                    continue;
+                }
+
+                let shortcutLevel = this.shortcutMap;
+                for(let key of shortObj.CODE.toUpperCase().split("+")) {
+                    shortcutLevel = shortcutLevel[key] = shortcutLevel[key] || {};
+                }
+
+                if(shortcutLevel.ACTION) {
+                    console.log(`${this.idKey}: Double registered shortcut: ${shortObj.CODE}`);
+                }
+                else {
+                    shortcutLevel.ACTION = shortObj.HANDLER;
+                }
+            }
+        }
+
+        onKeyDown = (event) => {
+            this.currentLevel = this.currentLevel || this.shortcutMap;
+            this.currentLevel = this.currentLevel[event.key.toUpperCase()];
+            if(this.currentLevel?.ACTION) {
+                eval(this.currentLevel.ACTION);
+                this.currentLevel = null;
+                event.stopPropagation();
+                event.preventDefault();
+            }
+        };
+
+        onKeyUp = (event) => {
+            if(!event.altKey && !event.shiftKey && !event.ctrlKey && !event.metaKey) {
+                this.currentLevel = null;
+            }
+        };
+    };
+    customElements.define("gw-shortcuts", ns.ShortcutsEl);
+}) (window.GW.Components.Shortcuts = window.GW.Components.Shortcuts || {});
