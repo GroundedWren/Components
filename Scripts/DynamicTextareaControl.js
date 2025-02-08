3d5a0c1c172c4f6888c5abea16b725db31f80476
Kathryn Aulabaugh <kathryn.aulabaugh@gmail.com>
2025-02-08 13:34:45 -0600

    Adds shadow dom support to dynTxa

--- Scripts/DynamicTextareaControl.js
+++ Scripts/DynamicTextareaControl.js
@@ -7,101 +7,134 @@ window.GW = window.GW || {};
 (function Controls(ns) {
 	ns.DynamicTextareaEl = class DynamicTextareaEl extends HTMLElement {
 		static InstanceCount = 0;
+		static InstanceMap = {};
 		static EditorMode = false;
-		static AsiPolite = null;
+		static Name = "gw-dynamic-textarea";
+		static Style = `${DynamicTextareaEl.Name} {
+			position: relative;
+
+			[id$="asiInstruct"] {
+				display: none;
+			}
+
+			label.mode-toggle {
+				user-select: none;
+
+				position: absolute;
+				top: 0;
+				right: 2ch;
+				z-index: 999;
+
+				display: grid;
+				grid-auto-flow: column;
+				gap: 2px;
+				align-items: center;
+				justify-content: end;
+
+				margin: 3px;
+
+				background-color: var(--background-color-2, #F2F2F2);
+				svg {
+					align-self: center;
+					width: 0.8em;
+					aspect-ratio: 1 / 1;
+					padding: 1px;
+
+					path {
+						fill: var(--icon-color);
+					}
+
+					&.checked {
+						display: none;
+					}
+					&.not-checked {
+						display: block;
+					}
+				}
+				&.checked {
+					background-color: var(--selected-color, #90CBDB);
+					svg {
+						&.checked {
+							display: block;
+						}
+						&.not-checked {
+							display: none;
+						}
+					}
+				}
+				span:has(> kbd) {
+					display: inline-flex;
+					align-items: baseline;
+					kbd {
+						display: inline-block;
+					}
+				}
+			}
+
+			textarea {
+				padding-block-start: 1.4rem;
+				min-width: 22ch;
+			}
+
+			&:not(:focus-within, :hover) {
+				label.mode-toggle {
+					display: none;
+				}
+			}
+		}
+		[dir="rtl"] ${DynamicTextareaEl.Name} label.mode-toggle {
+			left: 2ch;
+			right: auto;
+		}
+		`;
 
 		InstanceId;
 		IsInitialized;
 
 		TabBuffer = [];
 		MessageIdx = 0;
+		AsiPolite = null;
 
 		constructor() {
 			super();
-			this.InstanceId = DynamicTextareaEl.InstanceCount++;
-
-			if(this.InstanceId === 0) {
-				ns.DynamicTextareaEl.EditorMode = localStorage.getItem("gw-dynamic-textarea-editor-mode") === "on";
-				document.head.insertAdjacentHTML("beforeend", `
-				<style>
-					gw-dynamic-textarea {
-						position: relative;
-
-						[id$="asiInstruct"] {
-							display: none;
-						}
-
-						label.mode-toggle {
-							display: none;
-							user-select: none;
-
-							position: absolute;
-							top: 0;
-							right: 0;
-							z-index: 999;
-
-							display: grid;
-							grid-auto-flow: column;
-							gap: 2px;
-							align-items: center;
-							justify-content: end;
-
-							margin: 3px;
-
-							background-color: var(--background-color-2, #F2F2F2);
-							svg {
-								width: 0.8em;
-								aspect-ratio: 1 / 1;
-								padding: 1px;
-
-								path {
-									fill: var(--icon-color);
-								}
-
-								&.checked {
-									display: none;
-								}
-								&.not-checked {
-									display: block;
-								}
-							}
-							&.checked {
-								background-color: var(--selected-color, #90CBDB);
-								svg {
-									&.checked {
-										display: block;
-									}
-									&.not-checked {
-										display: none;
-									}
-								}
-							}
-						}
-
-						textarea {
-							padding-block-start: 1.4rem;
-						}
-
-						&:not(:focus-within, :hover) {
-							label.mode-toggle {
-								display: none;
-							}
-						}
-					}	
-				</style>`);
-
-				ns.DynamicTextareaEl.AsiPolite = document.createElement("aside");
-				ns.DynamicTextareaEl.AsiPolite.setAttribute("aria-live", "polite");
-				ns.DynamicTextareaEl.AsiPolite.setAttribute(
-					"style",
-					"position: absolute; left: -99999999px; top: 0px;"
-				);
-				document.body.insertAdjacentElement("beforeend", ns.DynamicTextareaEl.AsiPolite);
+			if(!this.getId) {
+				// We're not initialized correctly. Attempting to fix:
+				Object.setPrototypeOf(this, customElements.get(DynamicTextareaEl.Name).prototype);
 			}
+			this.InstanceId = DynamicTextareaEl.InstanceCount++;
+			if(this.InstanceId === 0) {
+				DynamicTextareaEl.EditorMode = localStorage.getItem(`${DynamicTextareaEl.Name}-editor-mode`) === "on";
+			}
+
+			this.AsiPolite = document.createElement("aside");
+			this.AsiPolite.setAttribute("aria-live", "polite");
+			this.AsiPolite.setAttribute(
+				"style",
+				"position: absolute; left: -99999999px; top: 0px;"
+			);
+		}
+
+		get Root() {
+			return this.getRootNode();
+		}
+		get Head() {
+			if(this.Root.head) {
+				return this.Root.head;
+			}
+			if(this.Root.getElementById("gw-head")) {
+				return this.Root.getElementById("gw-head");
+			}
+			const head = document.createElement("div");
+			head.setAttribute("id", "gw-head");
+			this.Root.prepend(head);
+			return head;
+		}
+		get IsInShadowDom() {
+			return this.Root.toString() === "[object ShadowRoot]";
 		}
 
 		getId(key) {
-			return `gw-dynamic-textarea-${this.InstanceId}-${key}`;
+			return `${DynamicTextareaEl.Name}-${this.InstanceId}-${key}`;
 		}
 		getRef(key) {
 			return this.querySelector(`#${this.getId(key)}`);
@@ -112,6 +145,15 @@ window.GW = window.GW || {};
 		}
 
 		connectedCallback() {
+			if(!this.Root.querySelector(`style.${DynamicTextareaEl.Name}`)) {
+				this.Head.insertAdjacentHTML(
+					"beforeend",
+					`<style class=${DynamicTextareaEl.Name}>${DynamicTextareaEl.Style}</style>`
+				);
+			}
+			this.insertAdjacentElement("afterend", this.AsiPolite);
+
+			DynamicTextareaEl.InstanceMap[this.InstanceId] = this;
 			if(!this.IsInitialized) {
 				const observer = new MutationObserver((_mutationList, _observer) => {});
 				observer.observe(this, {attributes: true, childList: false, subtree: false});
@@ -123,6 +165,13 @@ window.GW = window.GW || {};
 					this.renderContent();
 				}
 			}
+			else {
+				this.updateState();
+			}
+		}
+
+		disconnectedCallback() {
+			delete DynamicTextareaEl.InstanceMap[this.InstanceId];
 		}
 
 		renderContent = () => {
@@ -131,7 +180,7 @@ window.GW = window.GW || {};
 				<label id="${this.getId("lblToggle")}" class="mode-toggle" aria-hidden="true">
 					<svg class="not-checked" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. --><path d="M384 80c8.8 0 16 7.2 16 16V416c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V96c0-8.8 7.2-16 16-16H384zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z"/></svg>
 					<svg class="checked" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. --><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM337 209L209 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L303 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg>
-					Editor Mode (<kbd>F2</kbd>)
+					<span>Editor Mode</span> <span>(<kbd>F2</kbd>)</span>
 				</label>
 			`);
 
@@ -150,23 +199,23 @@ window.GW = window.GW || {};
 
 		onToggleClick = (event) => {
 			event.stopPropagation();
-			ns.DynamicTextareaEl.EditorMode = !ns.DynamicTextareaEl.EditorMode;
-			localStorage.setItem("gw-dynamic-textarea-editor-mode", ns.DynamicTextareaEl.EditorMode ? "on" : "off");
+			DynamicTextareaEl.EditorMode = !DynamicTextareaEl.EditorMode;
+			localStorage.setItem(`${DynamicTextareaEl.Name}-editor-mode`, DynamicTextareaEl.EditorMode ? "on" : "off");
 
-			ns.DynamicTextareaEl.AsiPolite.insertAdjacentHTML("afterbegin", `
+			this.AsiPolite.insertAdjacentHTML("afterbegin", `
 				<article id="${this.getId("msg-" + ++this.MessageIdx)}">
-					Tab now ${ns.DynamicTextareaEl.EditorMode  ? "adjusts indentation" : "moves focus"}
+					Tab now ${DynamicTextareaEl.EditorMode  ? "adjusts indentation" : "moves focus"}
 				</article>
 			`);
 			setTimeout((idx) => {
-				document.getElementById(this.getId("msg-" + idx)).remove();
+				this.AsiPolite.querySelector(`#${this.getId("msg-" + idx)}`).remove();
 			}, 100, this.MessageIdx);
 
-			Array.from(document.querySelectorAll("gw-dynamic-textarea")).forEach(element => element.updateState());
+			Object.values(DynamicTextareaEl.InstanceMap).forEach(element => element.updateState());
 		};
 
 		updateState() {
-			if(ns.DynamicTextareaEl.EditorMode) {
+			if(DynamicTextareaEl.EditorMode) {
 				this.getRef("lblToggle").classList.add("checked");
 				this.getRef("asiInstruct").innerText = "Editor mode is enabled, press F2 to toggle."
 			}
@@ -192,7 +241,7 @@ window.GW = window.GW || {};
 				return;
 			}
 
-			if(ns.DynamicTextareaEl.EditorMode) {
+			if(DynamicTextareaEl.EditorMode) {
 				if(event.key === "Tab") {
 					this.onTxaTab(event);
 					return;
@@ -278,6 +327,8 @@ window.GW = window.GW || {};
 			event.preventDefault();
 		};
 	}
-	customElements.define("gw-dynamic-textarea", ns.DynamicTextareaEl);
+	if(!customElements.get(ns.DynamicTextareaEl.Name)) {
+		customElements.define(ns.DynamicTextareaEl.Name, ns.DynamicTextareaEl);
+	}
 }) (window.GW.Controls = window.GW.Controls || {});
 GW?.Controls?.Veil?.clearDefer("GW.Controls.DynamicTextareaEl");
