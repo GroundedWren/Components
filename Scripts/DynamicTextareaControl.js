da9de9ba7b151bd23d92e66fce8bd34adb8df599
Kathryn Aulabaugh <kathryn.aulabaugh@gmail.com>
2025-05-04 14:32:42 -0500

    Fixes dyntxa focus-visible

--- Scripts/DynamicTextareaControl.js
+++ Scripts/DynamicTextareaControl.js
@@ -16,7 +16,6 @@ window.GW = window.GW || {};
 			box-sizing: border-box;
 			display: flex;
 			width: fit-content;
-			overflow: clip;
 
 			[id$="asiInstruct"] {
 				display: none;
@@ -107,14 +106,22 @@ window.GW = window.GW || {};
 				border-width: 2px;
 				border-style: groove;
 			}
-			code {
+			.clipper {
 				position: absolute;
 				z-index: -1;
-				opacity: 0;
-				color: #FFFFFF;
-				user-select: none;
-				white-space: pre-wrap;
-				border-color: transparent;
+				overflow: clip;
+				width: 100%;
+				height: 100%;
+				
+				code {
+					position: absolute;
+					z-index: -1;
+					opacity: 0;
+					color: #FFFFFF;
+					user-select: none;
+					white-space: pre-wrap;
+					border-color: transparent;
+				}
 			}
 			textarea {
 				scrollbar-width: none;
@@ -330,7 +337,7 @@ window.GW = window.GW || {};
 					<svg class="checked" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. --><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM337 209L209 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L303 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg>
 					<span>Editor Mode</span> <span>(<kbd>F2</kbd>)</span>
 				</label>
-				<code aria-hidden="true" inert></code>
+				<div class="clipper"><code aria-hidden="true" inert></code></div>
 			`);
 
 			this.getRef("lblToggle").addEventListener("click", this.onToggleClick);
