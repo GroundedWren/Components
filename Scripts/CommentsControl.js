c5ac0ecfd81217c78693166ff0923cbb3c3bc6de
Kathryn Aulabaugh <kathryn.aulabaugh@gmail.com>
2025-10-20 19:57:06 -0500

    Bundles dependencies into the comments control

--- Scripts/CommentsControl.js
+++ Scripts/CommentsControl.js
@@ -6,6 +6,192 @@
 
 window.GW = window.GW || {};
 (function Controls(ns) {
+	const styleSheet = new CSSStyleSheet();
+	styleSheet.replaceSync(`
+		gw-comment-form, gw-comment-list {
+			.input-horizontal-flex {
+				display: flex;
+				flex-direction: row;
+				flex-wrap: wrap;
+				justify-content: space-evenly;
+				align-items: center;
+				row-gap: 4px;
+			}
+			
+			.input-vertical {
+				display: flex;
+				flex-direction: column;
+				justify-content: flex-start;
+				align-items: flex-start;
+				margin-left: 5px;
+				margin-right: 5px;
+				
+				> label {
+					padding-bottom: 2px;
+				}
+			}
+			
+			.sr-only {
+				position: absolute;
+				left: -99999999px;
+			}
+		}
+
+		gw-comment-form {
+			.comment-form input {
+				width: 135px;
+			}
+			
+			.comment-box-container {
+				margin-top: 5px;
+				margin-left: auto;
+				margin-right: auto;
+				width: fit-content;
+			}
+			
+			.comment-form-title {
+				display: flex;
+				justify-content: center;
+				margin-bottom: 5px;
+				font-size: 1.25em;
+			}
+			
+			.inline-banner-wrapper {
+				display: flex;
+				
+				> .inline-banner {
+					width: 100%;
+				}
+			}
+			
+			.inline-banner {
+				margin: 10px;
+				border: 1px solid var(-border-color, black);
+				background-color: var(--banner-color, lightgrey);
+				color: var(--text-color, black);
+				padding: 10px;
+				word-break: break-word;
+				display: flex;
+				align-items: center;
+				gap: 5px;
+				
+				&.warning {
+					background-color: var(--banner-warn-color, #EDC200);
+				}
+			}
+
+			form {
+				border: 1px solid;
+				padding: 5px;
+				border-color: var(--border-color, black);
+				width: fit-content;
+				background-color: var(--background-color, white);
+				
+				> h1, > h2, > h3, > h4, > h5, > h6 {
+					margin: 0;
+					text-align: center;
+				}
+			}
+			
+			.form-footer {
+				display: flex;
+				flex-direction: row;
+				justify-content: flex-end;
+				
+				> * {
+					margin: 3px;
+					min-height: 30px;
+				}
+			}
+		}
+
+		gw-comment-list {
+		.comments-container {
+				display: flex;
+				flex-direction: column;
+				align-items: stretch;
+				max-width: 500px;
+				gap: 5px;
+			}
+		}
+
+		gw-comment-card {
+			.comment-article {
+				border-left: 2px solid var(--border-color, black);
+				padding: 4px;
+				background-color: var(--background-color, white);
+				display: flex;
+				flex-direction: column;
+				
+				blockquote {
+					max-width: unset !important;
+					overflow-wrap: break-word;
+				}
+				
+				button {
+					max-width: fit-content;
+					height: 30px;
+				}
+				
+				.comment-article {
+					margin-inline-start: 10px;
+					margin-block-start: 10px;
+					padding-inline-end: 0px;
+				}
+			}
+			
+			.comment-header {
+				display: grid;
+				grid-template-columns: 0fr 1fr 1fr;
+				gap: 5px;
+				align-items: baseline;
+			}
+			
+			.comment-id, .comment-header-right > time	{
+				font-size: 0.85em;
+				font-weight: lighter;
+				font-style: italic;
+				word-break: keep-all;
+			}
+			
+			.comment-header-right {
+				display: flex;
+				flex-direction: row;
+				justify-content: flex-end;
+			}
+			
+			.commenter-name {
+				font-size: 1.1em;
+				font-weight: bold;
+			}
+			
+			.comment-footer {
+				display: flex;
+				flex-direction: row;
+				gap: 5px;
+			}
+			
+			&:not(.collapsed) .show-comment {
+				display: none;
+			}
+			
+			&.collapsed {
+				.comment-article > *:not(.comment-header) {
+					display: none !important;
+				}
+				
+				.show-comment {
+					display: block;
+				}
+				
+				.comment-header-right time {
+					display: none;
+				}
+			}
+		}
+	`);
+	document.adoptedStyleSheets.push(styleSheet);
+
 	ns.CommentForm = class CommentForm extends HTMLElement {
 		//#region staticProperties
 		static instanceCount = 0;
@@ -19,6 +205,7 @@ window.GW = window.GW || {};
 		discordURL;
 		encodedPath;
 		fallbackEmail;
+		omitStar;
 
 		//#region element properties
 		formEl;
@@ -53,6 +240,7 @@ window.GW = window.GW || {};
 			this.discordURL = this.getAttribute("discordURL");
 			this.encodedPath = this.getAttribute("encodedPath");
 			this.fallbackEmail = this.getAttribute("fallbackEmail");
+			this.omitStar = this.hasAttribute("omitStar");
 
 			this.renderContent();
 			this.registerHandlers();
@@ -72,7 +260,7 @@ window.GW = window.GW || {};
 				<span id="${this.idKey}-title" class="comment-form-title">${this.titleText}</span>
 				<div class="input-horizontal-flex">
 					<div class="input-vertical">
-						<label for="${this.idKey}-dispName">Display name*</label>
+						<label for="${this.idKey}-dispName">Display name${this.omitStar ? "" : "*"}</label>
 						<input id="${this.idKey}-dispName" type="text" maxlength="1000" required="true">
 					</div>
 					<div class="input-vertical">
@@ -90,7 +278,7 @@ window.GW = window.GW || {};
 				</div>
 				<div class="comment-box-container">
 					<div class="input-vertical">
-						<label for="${this.idKey}-comment">Comment*</label>
+						<label for="${this.idKey}-comment">Comment${this.omitStar ? "" : "*"}</label>
 						<textarea  id="${this.idKey}-comment"
 							minlength="1"
 							maxlength="1000"
@@ -101,7 +289,11 @@ window.GW = window.GW || {};
 					</div>
 				</div>
 				<div id="${this.idKey}-banner" class="inline-banner" aria-live="polite">
-					<gw-icon iconKey="circle-info" title="info"></gw-icon>
+					<svg viewBox="0 0 512 512" role="img" style="width: 16px; height: 16px;">
+						<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. -->
+						<title>info</title>
+						<path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"></path>
+					</svg>
 					<span>Comments are manually approved</span>
 				</div>
 				<div class="form-footer">
@@ -255,7 +447,7 @@ window.GW = window.GW || {};
 			</div>
 			`
 
-			const sheetReader = new GW.Gizmos.GoogleSheetsReader(this.gSpreadsheetId, this.gSheetId);
+			const sheetReader = new GoogleSheetsReader(this.gSpreadsheetId, this.gSheetId);
 			await sheetReader.loadData();
 			this.innerHTML = "";
 
@@ -525,4 +717,134 @@ window.GW = window.GW || {};
 	};
 	customElements.define("gw-comment-card", ns.CommentCard);
 	GW?.Controls?.Veil?.clearDefer("GW.Controls.CommentCard");
+
+	class GoogleSheetsReader {
+		//setResponse is intended for React - it's how google responds to our GET. Fortunately valid JSON is inside this call, so we can just parse it out.
+		static #RESPONSE_PREFIX = "setResponse(";
+		static #RESPONSE_SUFFIX = ");";
+
+		//Here we can define any custom types based on column label. "Timestamp" is intended for ISO 8601 format date/time strings.
+		static #CUSTOM_LABEL_TYPES = {
+			"Timestamp": "timestamp"
+		};
+
+		spreadsheetId; //The ID of the spreadsheet. This is the part just after /d/ in the docs.google.com URL
+		sheetName; //The name of the particular sheet we're after
+
+		#sheetURL; //Composed request URL
+
+		loadPromise = null; //A promise created when loading begins and which resolves when data has finished loading.
+		tableJSON = null; //Raw JS Object version of the returned JSON.
+		rowData = null; //Parsed row data
+		colData = null; //A shortcut to the the column data in tableJSON
+		colIndex = null; //An index from column label to its metadata, plus array position
+
+		/**
+		 * Constructs a GoogleSheetsReader object
+		 * spreadsheetId is the part of the docs.google.com URL just after /d/
+		 * sheetName is the name of the particular page
+		 */
+		constructor(spreadsheetId, sheetName) {
+			this.spreadsheetId = spreadsheetId;
+			this.sheetName = sheetName;
+			this.#sheetURL = `https://docs.google.com/spreadsheets/d/${spreadsheetId}/gviz/tq?sheet=${sheetName}`;
+		}
+
+		/**
+		 * Loads and parses sheet data via HTTP GET
+		 * Returns null on success, and an error string on failure.
+		 */
+		async loadData() {
+			this.loadPromise = this.#loadData();
+			return this.loadPromise;
+		}
+
+		async #loadData() {
+			this.tableJSON = null;
+			this.rowData = null;
+			this.colData = null;
+			this.colIndex = null;
+
+			const response = await fetch(this.#sheetURL);
+			if (response.ok) {
+				return response.text().then((unparsedData) => {
+					//This is parsing out the valid JSON from the React method they gave us
+					const targetData = unparsedData.split(
+						GoogleSheetsReader.#RESPONSE_PREFIX
+					)[1].split(
+						GoogleSheetsReader.#RESPONSE_SUFFIX
+					)[0];
+
+					this.tableJSON = GoogleSheetsReader.#applyCustomLabelTypes(JSON.parse(targetData).table);
+					this.rowData = GoogleSheetsReader.#parseAllRows(this.tableJSON);
+					this.colIndex = GoogleSheetsReader.#indexColumns(this.tableJSON);
+					this.colData = this.tableJSON.cols;
+
+					return null;
+				});
+			}
+			else {
+				return response.statusText || response.status;
+			}
+		}
+
+		/**
+		 * Overrides any google-returned column data types with custom ones based on label
+		 */
+		static #applyCustomLabelTypes(tableJSON) {
+			tableJSON.cols.forEach(col => {
+				if(this.#CUSTOM_LABEL_TYPES[col.label]) {
+					col.type = this.#CUSTOM_LABEL_TYPES[col.label]
+				};
+			});
+			return tableJSON;
+		}
+
+		static #parseAllRows(tableJSON) {
+			const rowDataArray = [];
+			for(let i = 0; i < tableJSON.rows.length; i++) {
+				rowDataArray.push(this.#parseRow(i, tableJSON));
+			}
+			return rowDataArray;
+		}
+
+		static #parseRow(rowIdx, tableJSON) {
+			const rowData = {};
+			const cells = tableJSON.rows[rowIdx].c;
+
+			for(let i = 0; i < cells.length; i++) {
+				rowData[tableJSON.cols[i].label] = this.#parseCellType(cells[i], tableJSON.cols[i].type);
+			}
+			return rowData;
+		}
+
+		/**
+		 * Parses a cell based on its type. Further custom types will need their own parsing added here.
+		 */
+		static #parseCellType(cellData, cellType) {
+			switch (cellType) {
+				case "string":
+					return cellData ? cellData.v : cellData;
+				case "number":
+					return cellData ? cellData.v : cellData;
+				case "datetime":
+				case "date":
+					return (cellData && cellData.v) ? eval("new " + cellData.v) : null;
+				case "timestamp":
+					const cellTimestamp = new Date(cellData ? cellData.v : "");
+					return isNaN(cellTimestamp) ? null : cellTimestamp;
+				default:
+					return cellData;
+			}
+		}
+
+		static #indexColumns(tableJSON) {
+			const colIndex = {};
+			for(let i = 0; i < tableJSON.cols.length; i++) {
+				const colData = tableJSON.cols[i];
+				colIndex[colData.label] = {...colData, index: i};
+			}
+			return colIndex;
+		}
+	};
 }) (window.GW.Controls = window.GW.Controls || {});
